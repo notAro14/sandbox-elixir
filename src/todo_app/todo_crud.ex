ba66cbfb0236e188146605f1cb530d2811779eb0
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-03-08 14:58:28 +0100

    TodoList - updating entries

--- src/todo_app/todo_crud.ex
+++ src/todo_app/todo_crud.ex
@@ -14,4 +14,19 @@ defmodule TodoList do
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    %TodoList{entries: entries} = todo_list
+
+    case Map.fetch(entries, entry_id) do
+      {:error} ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(entries, entry_id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
 end
