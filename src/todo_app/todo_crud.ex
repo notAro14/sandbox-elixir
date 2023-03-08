5d97d2d2e996f0a5aa6e81639fa3b2f9a2db236c
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-03-08 15:30:57 +0100

    TodoList - iterative updates

--- src/todo_app/todo_crud.ex
+++ src/todo_app/todo_crud.ex
@@ -1,7 +1,13 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new(), do: %TodoList{}
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
 
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.auto_id)
